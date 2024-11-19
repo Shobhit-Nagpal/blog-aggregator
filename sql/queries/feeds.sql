b56a8c18367a42837f384db633db978280f18603
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-11-19 22:33:01 +0530

    add: create follow feed query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -12,3 +12,14 @@ RETURNING *;
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+-- name: CreateFeedFollow :many
+INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+VALUES (
+  $1,
+  $2,
+  $3,
+  $4,
+  $5,
+  )
+RETURNING *;
